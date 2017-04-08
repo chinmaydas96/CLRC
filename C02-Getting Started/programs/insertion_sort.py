94d120a19140947e5153883778b3019cbf2cf118
chinmaydas96 <chinmaydasbat@gmail.com>
2017-04-08 21:33:09 +0530

    chapter-2

---
+++ C02-Getting Started/programs/insertion_sort.py
@@ -0,0 +1,11 @@
+a = [14, 3, 7, 1, 5, 9, 2, 4]
+
+for j in range(1, len(a)):
+    key = a[j]
+    i = j - 1
+    while i >= 0 and a[i] > key:
+        a[i + 1] = a[i]
+        i = i - 1
+    a[i + 1] = key
+
+print a
